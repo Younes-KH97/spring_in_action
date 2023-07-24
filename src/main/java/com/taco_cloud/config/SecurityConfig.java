c86615dcc33385078b37e3c0b452324acee06389
Younes.khe97 <younes.khellaf97@gmail.com>
2023-07-24 10:15:53 +0100

    security config

---
+++ src/main/java/com/taco_cloud/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.taco_cloud.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.OPTIONS).permitAll()
+                            .requestMatchers(HttpMethod.POST, "/api/ingredients").permitAll()
+                            .requestMatchers("/api/tacos", "/api/orders/**").permitAll()
+                            .requestMatchers(HttpMethod.PATCH, "/api/ingredients").permitAll()
+                            .requestMatchers("/**").permitAll())
+                .formLogin()
+                .loginPage("/login")
+
+                .and()
+                .httpBasic()
+                .realmName("Taco Cloud")
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .csrf()
+                .ignoringRequestMatchers("/h2-console/**", "/api/**")
+
+                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin().and().build();
+
+    }
+}
