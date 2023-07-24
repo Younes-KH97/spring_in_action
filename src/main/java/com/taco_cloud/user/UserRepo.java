1ce9f5a9c7e879f76e19ad70f7a328316a3b3c4a
Younes.khe97 <younes.khellaf97@gmail.com>
2023-07-24 09:49:16 +0100

    application config

--- src/main/java/com/taco_cloud/user/UserRepo.java
+++ src/main/java/com/taco_cloud/user/UserRepo.java
@@ -3,7 +3,9 @@ package com.taco_cloud.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<UserApp, Long> {
-    UserApp findByUsername(String username);
+    Optional<UserApp> findByUsername(String username);
 }
