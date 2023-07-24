9999b695f55ecf1e717b0e00ae4a2d35fe244bf9
Younes.khe97 <younes.khellaf97@gmail.com>
2023-07-24 12:12:15 +0100

    update app config

--- src/main/java/com/taco_cloud/config/ApplicationConfig.java
+++ src/main/java/com/taco_cloud/config/ApplicationConfig.java
@@ -1,18 +1,23 @@
 package com.taco_cloud.config;
 
+import com.taco_cloud.data.IngredientRepository;
+import com.taco_cloud.domain.Ingredient;
+import com.taco_cloud.user.UserApp;
 import com.taco_cloud.user.UserRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
@@ -47,4 +52,35 @@ public class ApplicationConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(
+            IngredientRepository ingredientRepository,
+            UserRepo userRepo
+            ) {
+        return args -> {
+            ingredientRepository.saveAll(List.of(
+                            Ingredient.builder().id("FLTO").name("Flour Tortilla").type(Ingredient.Type.WRAP).build(),
+                            Ingredient.builder().id("COTO").name("Corn Tortilla").type(Ingredient.Type.WRAP).build(),
+                            Ingredient.builder().id("GRBF").name("Ground Beef").type(Ingredient.Type.PROTEIN).build(),
+                            Ingredient.builder().id("CARN").name("Carnitas").type(Ingredient.Type.PROTEIN).build(),
+                            Ingredient.builder().id("TMTO").name("Diced Tomatoes").type(Ingredient.Type.VEGGIES).build(),
+                            Ingredient.builder().id("LETC").name("Lettuce").type(Ingredient.Type.VEGGIES).build(),
+                            Ingredient.builder().id("CHED").name("Cheddar").type(Ingredient.Type.CHEESE).build(),
+                            Ingredient.builder().id("JACK").name("Monterrey Jack").type(Ingredient.Type.CHEESE).build(),
+                            Ingredient.builder().id("SLSA").name("Salsa").type(Ingredient.Type.SAUCE).build()
+                    )
+            );
+            userRepo.saveAll(List.of(
+                    UserApp.builder().username("user_1")
+                                     .fullname("user_1")
+                                     .state("state_1")
+                                     .city("city_1")
+                                     .phoneNumber("111")
+                                     .zip("zip_1")
+                                     .street("street_1")
+                                     .password(passwordEncoder().encode("user_123"))
+                            .build()
+            ));
+        };
+    }
 }
